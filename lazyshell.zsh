998265c34ae91c6307dd393b8abfa79c0f1a318c
psyf <gzsaif001@gmail.com>
2023-03-03 17:13:00 +0800

    modified to use chatGPT API - faster and lower cost

--- lazyshell.zsh
+++ lazyshell.zsh
@@ -21,20 +21,20 @@ __lazyshell_complete() {
   CURSOR=$#BUFFER
 
   if [[ -z "$buffer_context" ]]; then
-    local prompt="Write a bash command for query: \`$REPLY\`. Answer with the command only."
+    local prompt="Write a zsh command for query: \`$REPLY\`. Answer with a single command only, without any additional characters so it can be pasted into the terminal."
   else
-    local prompt="Alter bash command \`$buffer_context\` to comply with query \`$REPLY\`. Answer with the command only."
+    local prompt="Alter zsh command \`$buffer_context\` to comply with query \`$REPLY\`. Answer with a single command only, without any additional characters so it can be pasted into the terminal."
   fi
 
   # todo: better escaping
   local escaped_prompt=$(echo "$prompt" | sed 's/"/\\"/g' | sed 's/\n/\\n/g')
-  local data='{"prompt":"'"$escaped_prompt"'","model":"text-davinci-003","max_tokens":256,"temperature":0}'
+  local data='{"messages":[{"role": "system","content":"You are the world leading expert on zsh shell commands for a computer running on Ubuntu 22.04.2 LTS. Always make the best guess if you do not have enough information. "},{"role":"user","content":"'"$escaped_prompt"'"}],"model":"gpt-3.5-turbo","max_tokens":256,"temperature":0.7}'  
 
   # Display a spinner while the API request is running in the background
   local spinner=("⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏")
   set +m
   local response_file=$(mktemp)
-  { curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $OPENAI_API_KEY" --data "$data" https://api.openai.com/v1/completions > "$response_file" } &>/dev/null &
+  { curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $OPENAI_API_KEY" -d "$data" https://api.openai.com/v1/chat/completions > "$response_file" } &>/dev/null &
   local pid=$!
   while true; do
     for i in "${spinner[@]}"; do
@@ -58,7 +58,7 @@ __lazyshell_complete() {
   local response=$(cat "$response_file")
   rm "$response_file"
 
-  local generated_text=$(echo -E $response | jq -r '.choices[0].text' | xargs)
+  local generated_text=$(echo -E $response | jq -r '.choices[0].message.content' | xargs -0)
   local error=$(echo -E $response | jq -r '.error.message')
 
   if [ $? -ne 0 ]; then
