8db829d2ef9c806c2930117efc0d6e6cfd7e2eea
not-poma <semenovroma@gmail.com>
2023-03-08 11:24:45 +0600

    fallback to wget if curl is unavailable

--- lazyshell.zsh
+++ lazyshell.zsh
@@ -26,6 +26,20 @@ __lzsh_preflight_check() {
     zle reset-prompt
     return 1
   fi
+
+  if ! command -v jq &> /dev/null; then
+    echo ""
+    echo "Error: jq is not installed"
+    zle reset-prompt
+    return 1
+  fi
+
+  if ! command -v curl &> /dev/null && ! command -v wget &> /dev/null; then
+    echo ""
+    echo "Error: curl or wget is not installed"
+    zle reset-prompt
+    return 1
+  fi
 }
 
 __lzsh_llm_api_call() {
@@ -44,7 +58,11 @@ __lzsh_llm_api_call() {
   # Read the response from file
   # Todo: avoid using temp files
   set +m
-  { curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $OPENAI_API_KEY" -d "$data" https://api.openai.com/v1/chat/completions > "$response_file" } &>/dev/null &
+  if command -v curl &> /dev/null; then
+    { curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $OPENAI_API_KEY" -d "$data" https://api.openai.com/v1/chat/completions > "$response_file" } &>/dev/null &
+  else
+    { wget -qO- --header="Content-Type: application/json" --header="Authorization: Bearer $OPENAI_API_KEY" --post-data="$data" https://api.openai.com/v1/chat/completions > "$response_file" } &>/dev/null &
+  fi
   local pid=$!
 
   # Display a spinner while the API request is running in the background
